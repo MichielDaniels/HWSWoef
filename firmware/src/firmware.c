038711fca9fab7edc1734b29e0ab54aa27ce79ce
MichielDaniels <michiel.daniels@student.uhasselt.be>
2022-03-14 09:46:15 +0100

    ex301firmware

---
+++ firmware/src/firmware.c
@@ -0,0 +1,15 @@
+#include "print.h"
+#include "counter.h"
+
+void main(void) {
+
+	unsigned int counter_value;
+	volatile unsigned char temp;
+
+	print_str("count: ");
+	counter_start();
+	for(temp=0;temp<10;temp++);
+	counter_stop();
+	counter_value = counter_get_value();
+	print_hex(counter_value, 2);
+}
